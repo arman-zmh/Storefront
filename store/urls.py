8b96fdd1aaad3fe30ed240d69b4fc2d767942be5
arman-zmh <arman.zamaniha123@gmail.com>
2025-12-21 11:51:10 +0330

    nested rout and filtering

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
+#from rest_framework.routers import DefaultRouter
 from . import views
 
-router = DefaultRouter()
-router.register('products', views.ProductViewSet)
-router.register('collections', views.CollectionViewSet)
+router = routers.DefaultRouter()
+router.register('products', views.ProductViewSet, basename='products')
 
-urlpatterns = router.urls
+products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
+products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
+
+
+#router.register('products', views.ProductViewSet)
+#router.register('collections', views.CollectionViewSet)
+
+urlpatterns = router.urls + products_router.urls
--- store/views.py
+++ store/views.py
@@ -11,8 +11,15 @@ from .models import Product, Collection, OrderItem, Review
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 
 class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all()
+    #queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    def get_queryset(self):
+        query_set = Product.objects.all()
+        collection_id = self.request.query_params.get('collection_id')
+        if collection_id is not None:
+            query_set = Product.objects.filter(collection_id=collection_id)
+        return query_set
+
     def get_serializer_context(self):
         return {'request':self.request}
     
@@ -47,5 +54,9 @@ class CollectionViewSet(ModelViewSet):
         return Response(status=status.HTTP_204_NO_CONTENT)"""
     
 class ReviewViewSet(ModelViewSet):
-    queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    def get_queryset(self):
+        return Review.objects.filter(product_id=self.kwargs['product_pk'])
+    
+    def get_serializer_context(self):
+        return {'product_id': self.kwargs['product_pk']}
