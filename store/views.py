6643cd74eb38560676d2dbb9485e0017ae4cbe0f
arman-zmh <arman.zamaniha123@gmail.com>
2026-01-03 17:48:11 +0330

    Getting a cart

--- store/views.py
+++ store/views.py
@@ -5,7 +5,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 #from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.mixins import CreateModelMixin
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
 #from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 #from rest_framework.views import APIView
 from rest_framework.filters import SearchFilter, OrderingFilter
@@ -65,6 +65,6 @@ class ReviewViewSet(ModelViewSet):
     def get_serializer_context(self):
         return {'product_id': self.kwargs['product_pk']}
     
-class CartViewSet(CreateModelMixin, GenericViewSet):
+class CartViewSet(CreateModelMixin, GenericViewSet, RetrieveModelMixin):
+    queryset = Cart.objects.prefetch_related('items__product').all()
     serializer_class = CartSerializer
-    queryset = Cart.objects.all()
