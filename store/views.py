a9b2c9992cd3b706d977ed92e0adbaa8a7372a31
arman-zmh <arman.zamaniha123@gmail.com>
2026-01-28 12:48:52 +0330

    Creating user and profile

--- store/views.py
+++ store/views.py
@@ -5,16 +5,16 @@ from django_filters.rest_framework import DjangoFilterBackend
 #from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, UpdateModelMixin
 #from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 #from rest_framework.views import APIView
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 
-from .models import Product, Collection, OrderItem, Review, Cart, CartItem
+from .models import Product, Collection, OrderItem, Review, Cart, CartItem, Customer
 from .pagination import DefualtPagination
 from .filters import ProductFilter
-from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer, CartSerializer, CartItemSeriializer, AddCartItemSerializer, UpdateCartItemSerializer
+from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer, CartSerializer, CartItemSeriializer, AddCartItemSerializer, UpdateCartItemSerializer, CustomerSerializer
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
@@ -88,3 +88,8 @@ class CartItemViewSet(ModelViewSet):
     def get_queryset(self):
         return CartItem.objects.filter(cart_id=self.kwargs['cart_pk']) \
                 .select_related('product')    
+    
+
+class CustomerViewSet(CreateModelMixin,RetrieveModelMixin,UpdateModelMixin, GenericViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
