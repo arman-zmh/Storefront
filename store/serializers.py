a2660865a4daa8d63cd9e0f778619e5bad4f1acd
arman-zmh <arman.zamaniha123@gmail.com>
2026-02-10 15:24:51 +0330

    Raising error for empty carts

--- store/serializers.py
+++ store/serializers.py
@@ -109,6 +109,12 @@ class OrderSerializer(serializers.ModelSerializer):
 
 class CreateOrderSerializer(serializers.Serializer):
     cart_id = serializers.UUIDField()
+    def validate_cart_id(self, cart_id):
+        if not Cart.objects.filter(pk=cart_id).exists():
+            raise serializers.ValidationError('No cart with given id.')
+        if CartItem.objects.filter(cart_id=cart_id).count() == 0:
+            raise serializers.ValidationError('The cart is empty')
+        return cart_id
 
     def save(self, **kwargs):
         with transaction.atomic():
@@ -129,3 +135,5 @@ class CreateOrderSerializer(serializers.Serializer):
 
             OrderItem.objects.bulk_create(order_items)
             Cart.objects.filter(pk=cart_id).delete()
+
+            return order
